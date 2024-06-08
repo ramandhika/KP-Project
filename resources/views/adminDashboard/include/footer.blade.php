4bb2e34f458adeb2159ee037d015e9e79222d609
Ramandhika <ramandhika.ilham@gmail.com>
2024-06-08 20:51:32 +0700

    refactor: include footer in admin dashboard index view

---
+++ resources/views/adminDashboard/include/footer.blade.php
@@ -0,0 +1,24 @@
+    <!-- ======= Footer ======= -->
+    <footer id="footer" class="footer">
+        <div class="copyright">
+            &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+            Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a> | Developed by <a
+                href="{{ route('home') }}">Ramandhika Ilham Pratama, Cantika Putri Lylla, Fajar Tri Hartanto</a>
+        </div>
+    </footer><!-- End Footer -->
+
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+
+    <!-- Vendor JS Files -->
+    <script src="{{ asset('assets/vendorDashboardAdmin/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('assets/vendorDashboardAdmin/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('assets/vendorDashboardAdmin/tinymce/tinymce.min.js') }}"></script>
+    <!-- Template Main JS File -->
+    <script src="{{ asset('assets/jsDashboardAdmin/main.js') }}"></script>
+
+    </body>
+
+    </html>
